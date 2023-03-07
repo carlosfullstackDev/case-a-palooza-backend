11877b4d2d2c479c20a9325f3637f88d6a91c83d
carlosfullstackDev <127157564+carlosfullstackDev@users.noreply.github.com>
2023-03-06 22:00:11 -0600

    Update CaseAPaloozaProductServiceApplication.java

--- src/main/java/com/example/demo/CaseAPaloozaProductServiceApplication.java
+++ src/main/java/com/example/demo/CaseAPaloozaProductServiceApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CaseAPaloozaProductServiceApplication {
 
 	public static void main(String[] args) {
+		System.out.println("Hello world");
 		SpringApplication.run(CaseAPaloozaProductServiceApplication.class, args);
 	}
 
