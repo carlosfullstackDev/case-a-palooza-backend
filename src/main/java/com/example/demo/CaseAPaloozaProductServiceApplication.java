afe154440ea3c5bf9d1f1d1e43497a05e39e1dea
Carlos Torres <carlos@yugioh.com>
2023-03-09 21:47:42 -0600

    commit to trigger pipeline test

--- src/main/java/com/example/demo/CaseAPaloozaProductServiceApplication.java
+++ src/main/java/com/example/demo/CaseAPaloozaProductServiceApplication.java
@@ -11,6 +11,7 @@ public class CaseAPaloozaProductServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CaseAPaloozaProductServiceApplication.class, args);
+		System.out.println("prueba jenkins pipeline");
 	}
 
 }
